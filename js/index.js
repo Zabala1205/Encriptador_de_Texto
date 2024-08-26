f881cf0aeb669082a53c85d196c09935d78eeb5c
David <davids.zabalaa@ecci.edu.co>
2024-08-25 23:37:41 -0500

    Proyecto Encriptador

---
+++ js/index.js
@@ -0,0 +1,126 @@
+//----- Selección de Elementos -------//
+const btnEncriptar = document.querySelector(".btn-encriptar");
+const txtEncriptar = document.querySelector (".encriptar");
+const aviso = document.querySelector(".texto-aviso");
+const respuesta = document.querySelector(".evaluar");
+const contenido = document.querySelector (".tarjeta-contenedor");
+const btnCopiar = document.querySelector(".btn-copiar");
+const btnDesencriptar = document.querySelector(".btn-desencriptar");
+
+//------- Botón de Encriptar -------//
+
+btnEncriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g,"")
+    
+    if (texto == ""){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontweight = "800";
+        aviso.textContent = "El campo de texto no debe estar vacio";
+
+        setTimeout(()=>{
+            aviso.removeAttribute ("style");
+        },1500);
+    }
+
+    else if (texto !== txt){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontweight = "800";
+        aviso.textContent = "El texto no debe tener acentos y caracteres especiales";
+    
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);      
+    }
+
+    else if (texto !== texto.toLowerCase()){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontweight = "800";
+        aviso.textContent = "El texto debe ser todo en minúscula";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else{
+        texto = texto.replace(/e/mg, "enter");
+        texto = texto.replace(/i/mg, "imes");
+        texto = texto.replace(/a/mg, "ai");
+        texto = texto.replace(/o/mg, "over");
+        texto = texto.replace(/u/mg, "ufat");
+
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove();
+    }
+    
+});
+
+//------- Botón Desencriptar -------//
+
+btnDesencriptar.addEventListener("click", e=>{
+    e.preventDefault();
+    let texto = txtEncriptar.value;
+    let txt = texto.normalize("NFD").replace(/[\.¿\?~!\¡@#%^&*()_|}\{[\]>\<:"`;,\u0300-\u036f']/g,"")
+    
+    if (texto == ""){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontweight = "800";
+        aviso.textContent = "El campo de texto no debe estar vacio";
+
+        setTimeout(()=>{
+            aviso.removeAttribute ("style");
+        },1500);
+    }
+
+    else if (texto !== txt){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontweight = "800";
+        aviso.textContent = "El texto no debe tener acentos y caracteres especiales";
+    
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);      
+    }
+
+    else if (texto !== texto.toLowerCase()){
+        aviso.style.background = "#0A3871";
+        aviso.style.color = "#FFFF";
+        aviso.style.fontweight = "800";
+        aviso.textContent = "El texto debe ser todo en minúscula";
+
+        setTimeout(()=>{
+            aviso.removeAttribute("style");
+        },1500);
+    }
+
+    else{
+        texto = texto.replace(/enter/mg, "e");
+        texto = texto.replace(/imes/mg, "i");
+        texto = texto.replace(/ai/mg, "a");
+        texto = texto.replace(/over/mg, "o");
+        texto = texto.replace(/ufat/mg, "u");
+
+        respuesta.innerHTML = texto;
+        btnCopiar.style.visibility = "inherit";
+        contenido.remove();
+    }
+    
+});
+
+
+//------- Botón Copiar -------//
+
+btnCopiar.addEventListener("click", e=>{
+    e.preventDefault();
+    let copiar = respuesta;
+    copiar.select();
+    document.execCommand("copy");
+});
